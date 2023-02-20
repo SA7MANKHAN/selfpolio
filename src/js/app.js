895d8a8d2a9622f2cd98554091dd387ff19f22d5
Salman <salmanwbillo@gmail.com>
2023-02-20 08:49:40 +0500

    Initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
-//
+const burger = document.querySelector('.burger')
+const sidebar = document.querySelector('#sidebar')
+const links = document.querySelectorAll('#sidebar ul li a')
+
+burger.addEventListener('click', ()=> {
+    sidebar.classList.toggle('is-active')
+    burger.classList.toggle('is-active')
+})
+
+links.forEach(link => link.addEventListener('click', ()=>{
+    links.forEach(link => link.classList.remove('is-active'))
+    link.classList.add('is-active')
+
+    sidebar.classList.remove('is-active')
+    burger.classList.remove('is-active')
+}))
+
+// AOS
+AOS.init({
+    duration: 900,
+});
